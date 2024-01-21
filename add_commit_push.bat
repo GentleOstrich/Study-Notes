e8d1c2f003b418c1da71d033b61ee3d58e60aec4
GentleOstrich <273819111@qq.com>
2024-01-21 18:49:58 +0800

    update

--- add_commit_push.bat
+++ add_commit_push.bat
@@ -1,19 +1,19 @@
 @echo off
 git status | find "nothing to commit" > nul && (
-	echo=
 	echo No changes to commit.
+	echo=
 ) || (
 	git add . && git commit -m "update" && (
-		echo=
 		echo commit success!!
+		echo=
 	)
 )
 git push && (
-    echo=
     echo push success!!
+	echo=
 ) || (
-    echo=
     echo push failed.
+	echo=
 )
 
 
